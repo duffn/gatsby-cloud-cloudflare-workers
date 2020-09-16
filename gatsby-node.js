e98d845def167d9f543e4ad92c94e3bd8245dce2
Nicholas Duffy <duffn@outlook.com>
2020-09-16 15:59:04 -0600

    Add conditional

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,17 @@
-// const execa = require("execa")
 const { spawn } = require("child_process")
 
 exports.onPostBuild = async () => {
   const PRODUCTION_BRANCH = `master`
   const currentBranch = process.env.BRANCH
-  console.log("exports.onPostBuild -> currentBranch", currentBranch)
 
-  // if (currentBranch === PRODUCTION_BRANCH) {
-  const child = spawn(`./node_modules/.bin/wrangler`, [`publish`])
-  for await (const data of child.stdout) {
-    console.log(data.toString())
+  if (currentBranch === PRODUCTION_BRANCH) {
+    const child = spawn(`./node_modules/.bin/wrangler`, [`publish`])
+    for await (const data of child.stdout) {
+      console.log(data.toString())
+    }
+  } else {
+    console.log(
+      `Skipping deploy to Cloudflare. ${currentBranch} does not match ${PRODUCTION_BRANCH} production branch.`
+    )
   }
-  // const { stdout } = await execa("./node_modules/.bin/wrangler", ["publish"])
-
-  // console.log(stdout)
-  // } else {
-  //   console.log(
-  //     `Skipping deploy to Cloudflare. On ${currentBranch}. Not on the ${PRODUCTION_BRANCH} production branch.`
-  //   )
-  // }
 }
