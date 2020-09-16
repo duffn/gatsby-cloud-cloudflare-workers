d66f0c98371d3d86ac9c2651545230d30d00b6b5
Nicholas Duffy <duffn@outlook.com>
2020-09-16 15:52:15 -0600

    Don't use execa

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,21 @@
-const execa = require("execa")
+// const execa = require("execa")
+const { spawn } = require("child_process")
 
 exports.onPostBuild = async () => {
-  const { stdout } = await execa("./node_modules/.bin/wrangler", ["publish"])
-  console.log("exports.onPostBuild -> stdout", stdout)
+  const PRODUCTION_BRANCH = `master`
+  const currentBranch = process.env.BRANCH
+
+  // if (currentBranch === PRODUCTION_BRANCH) {
+  const child = spawn(`./node_modules/.bin/wrangler`, [`publish`])
+  for await (const data of child.stdout) {
+    console.log(data)
+  }
+  // const { stdout } = await execa("./node_modules/.bin/wrangler", ["publish"])
+
+  // console.log(stdout)
+  // } else {
+  //   console.log(
+  //     `Skipping deploy to Cloudflare. On ${currentBranch}. Not on the ${PRODUCTION_BRANCH} production branch.`
+  //   )
+  // }
 }
