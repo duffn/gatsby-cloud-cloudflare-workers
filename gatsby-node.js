3aaa702e71811fdc57a2e7e278b3372ef8d4a640
Nicholas Duffy <duffn@outlook.com>
2020-09-16 16:04:12 -0600

    Only deploy when the branch is the production branch (#1)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,17 @@
-const execa = require("execa")
+const { spawn } = require("child_process")
 
 exports.onPostBuild = async () => {
-  const { stdout } = await execa("./node_modules/.bin/wrangler", ["publish"])
-  console.log("exports.onPostBuild -> stdout", stdout)
+  const PRODUCTION_BRANCH = `master`
+  const currentBranch = process.env.BRANCH
+
+  if (currentBranch === PRODUCTION_BRANCH) {
+    const child = spawn(`./node_modules/.bin/wrangler`, [`publish`])
+    for await (const data of child.stdout) {
+      console.log(data.toString())
+    }
+  } else {
+    console.log(
+      `Skipping deploy to Cloudflare. ${currentBranch} does not match ${PRODUCTION_BRANCH} production branch.`
+    )
+  }
 }
